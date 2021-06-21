a0d22763e6a25ffbee9909dee41bd66c13b57043
vilakshan-coder <73805875+vilakshan-coder@users.noreply.github.com>
2021-06-21 08:28:52 +0530

    security key removed

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import SrtipCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton=({price})=>{
     const priceForStripe=price*100;
-    const publishableKey='pk_test_51J4cZcSJ2eXuYij2KD99cKIIL3o1i2VdATgoqLgEqeyhzV4CoEXLnAoZeA57WcjoaH4UO5waIue0113SIExUwCyg00srs2Rf7F';
+    const publishableKey='';
     
    const onTaken=token=>{
         console.log(token);
